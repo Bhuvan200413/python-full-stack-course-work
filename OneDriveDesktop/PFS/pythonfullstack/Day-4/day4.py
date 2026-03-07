6f80ab405f4b46ea1644422b1d1b2ded680ba409
Bhuvan <bhuvan.kanukanti2004@gmail.com>
2026-03-07 17:34:54 +0530

    Add Day-3 and Day-4 course work

---
+++ OneDriveDesktop/PFS/pythonfullstack/Day-4/day4.py
@@ -0,0 +1,15 @@
+#condition statements
+'''data = ('user@gmail.com','user@123')
+mail = input('Enter the mail: ')
+password = input('Enter the password: ')
+
+if data == (mail,password) :
+    print('Login successful')
+else: 
+    print('Invalid login')'''
+fruits = ['mango','papaya','pine apple']
+search_item = input("search item: ")
+if search_item in fruits :
+    print('found item buy now')
+else:
+    print("item notfound: ")
